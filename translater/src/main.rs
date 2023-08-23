c64502b26b92eaaaeef414a5a970f1d021fdcd8f
cyejing <chenyejing@didiglobal.com>
2023-08-23 18:43:30 +0800

    add path

--- translater/src/main.rs
+++ translater/src/main.rs
@@ -1,8 +1,13 @@
+use std::path::PathBuf;
+
 use chatter::router::create_router;
 use sqlx::MySqlPool;
 
 #[shuttle_runtime::main]
-async fn axum(#[shuttle_aws_rds::MySql] pool: MySqlPool) -> shuttle_axum::ShuttleAxum {
+async fn axum(
+    #[shuttle_aws_rds::MySql] pool: MySqlPool,
+    #[shuttle_static_folder::StaticFolder(folder = "public")] _public: PathBuf,
+) -> shuttle_axum::ShuttleAxum {
     let app = create_router(pool).await;
     Ok(app.into())
 }
