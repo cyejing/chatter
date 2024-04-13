06012c07ba61d0b795d139052fea455ee633f57b
cyejing <chenyejing@didiglobal.com>
2024-04-14 02:43:40 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import TextInput from "./components/TextInput";
+
+function App() {
+  return (
+    <>
+      <div className="container mx-auto px-10 pt-10">
+        <TextInput></TextInput>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/TextInput.tsx
@@ -0,0 +1,7 @@
+export default function textInput() {
+  return (
+    <>
+      <textarea className="border-2 border-gray-500 w-full"></textarea>
+    </>
+  );
+}
